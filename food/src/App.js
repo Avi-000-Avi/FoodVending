ef9c91819651a3f07210c49d80d943d9368d1586
Avinash Toppo <atoppo267@gmail.com>
2022-04-11 17:30:25 +0530

    Products Page

---
+++ food/src/App.js
@@ -0,0 +1,16 @@
+import { Routes,Route } from 'react-router-dom';
+import Products from './components/products';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path = "/" element = {<Products/>} />
+        
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
