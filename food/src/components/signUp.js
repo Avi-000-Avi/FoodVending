b85256907195746288d00da56f2a7c3ba3201db5
Avinash Toppo <atoppo267@gmail.com>
2022-04-19 12:44:24 +0530

    Complete

--- food/src/components/signUp.js
+++ food/src/components/signUp.js
@@ -1,18 +1,15 @@
-import React from 'react';
+import React, { useEffect, useRef ,useState} from 'react';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import { makeStyles } from '@material-ui/core/styles';
 import {
   Container,
   Grid,
-  Typography
+  Typography,TextField
 } from '@material-ui/core';
-import Textfield from './FormsUI/Textfield';
-import Select from './FormsUI/Select';
-import DateTimePicker from './FormsUI/DateTimePicker';
-import Checkbox from './FormsUI/Checkbox';
+
 import Button from './FormsUI/Button';
-import countries from '../utils/countries.json';
+import { Pincode } from './pinCode';
 
 const useStyles = makeStyles((theme) => ({
   formWrapper: {
@@ -20,205 +17,92 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: theme.spacing(8),
   },
 }));
-
-const INITIAL_FORM_STATE = {
-  firstName: '',
-  lastName: '',
-  email: '',
-  phone: '',
-  addressLine1: '',
-  addressLine2: '',
-  city: '',
-  state: '',
-  country: '',
-  arrivealDate: '',
-  departureDate: '',
-  message: '',
-  termsOfService: false
+const recoganizedAreas = {
+  'hsr':'560102',
+  'bypanahalli':'676667',
+  'marathalli':'667776',
+  'hosurRoad':'321236',
+  'teachersColony':'560101'
 };
 
-const FORM_VALIDATION = Yup.object().shape({
-  firstName: Yup.string()
-    .required('Required'),
-  lastName: Yup.string()
-    .required('Required'),
-  email: Yup.string()
-    .email('Invalid email.')
-    .required('Required'),
-  phone: Yup.number()
-    .integer()
-    .typeError('Please enter a valid phone number')
-    .required('Required'),
-  addressLine1: Yup.string()
-    .required('Required'),
-  addressLine2: Yup.string(),
-  city: Yup.string()
-    .required('Required'),
-  state: Yup.string()
-    .required('Required'),
-  country: Yup.string()
-    .required('Required'),
-  arrivealDate: Yup.date()
-    .required('Required'),
-  departureDate: Yup.date()
-    .required('Required'),
-  message: Yup.string(),
-  termsOfService: Yup.boolean()
-    .oneOf([true], 'The terms and conditions must be accepted.')
-    .required('The terms and conditions must be accepted.'),
-});
+
 
 export const SignupForm = () => {
   const classes = useStyles();
 
-  return (
-    <Grid container>
+  function handleCHANGE({setFieldValue,values}){
+    console.log(values.address)
+  }
 
-      <Grid item xs={12}>
+  function hAddressChange ({address, currentValue, setFieldValue}){
+    console.log({address, currentValue});
+
+    if(currentValue){
+      const foundArea = Object.keys(recoganizedAreas).find(key => key === (currentValue))
+
+      const foundPinCode = recoganizedAreas[foundArea];
+      
+      setFieldValue("pincode", foundPinCode);
+      }else{
+      setFieldValue("pincode", "");
+      }
+    }
+
+  
+
+  return (
         <Container maxWidth="md">
           <div className={classes.formWrapper}>
 
             <Formik
-              initialValues={{
-                ...INITIAL_FORM_STATE
-              }}
-              validationSchema={FORM_VALIDATION}
+              initialValues={{address:'',pincode:''}}
+              enableReinitialize
               onSubmit={values => {
                 console.log(values);
               }}
-            >
-              <Form>
+              >
+            {props => {
+              const {
+                values,
+                
+                setFieldValue
+              } = props;
+              return (
+                <Form>
+                  <Grid container spacing={2}>
+                      <Grid item xs={12}>
+                        <Typography>
+                          Your details
+                        </Typography>
+                      </Grid>
 
-                <Grid container spacing={2}>
+                      <Grid item xs={6}>
+                        <TextField
+                          name="address"
+                          label="Address"
+                          onChange={(e) => { hAddressChange({currentValue: e.currentTarget.value, setFieldValue})}}
+                        />
+                        <ul>
+                      </ul>
+                      </Grid>
 
-                  <Grid item xs={12}>
-                    <Typography>
-                      Your details
-                    </Typography>
+                      <Grid item xs={6}>
+                        <TextField
+                          name="pincode"
+                          value={values.pincode}
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Button>
+                          Submit Form
+                        </Button>
+                      </Grid>
                   </Grid>
-
-                  <Grid item xs={6}>
-                    <Textfield
-                      name="firstName"
-                      label="First Name"
-                    />
-                  </Grid>
-
-                  <Grid item xs={6}>
-                    <Textfield
-                      name="lastName"
-                      label="Last Name"
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Textfield
-                      name="email"
-                      label="Email"
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Textfield
-                      name="phone"
-                      label="Phone"
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Typography>
-                      Address
-                    </Typography>
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Textfield
-                      name="addressLine1"
-                      label="Address Line 1"
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Textfield
-                      name="addressLine2"
-                      label="Address Line 2"
-                    />
-                  </Grid>
-
-                  <Grid item xs={6}>
-                    <Textfield
-                      name="city"
-                      label="City"
-                    />
-                  </Grid>
-
-                  <Grid item xs={6}>
-                    <Textfield
-                      name="state"
-                      label="State"
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Select
-                      name="country"
-                      label="Country"
-                      options={countries}
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Typography>
-                      Booking information
-                    </Typography>
-                  </Grid>
-
-                  <Grid item xs={6}>
-                    <DateTimePicker
-                      name="arrivealDate"
-                      label="Arrival Date"
-                    />
-                  </Grid>
-
-                  <Grid item xs={6}>
-                    <DateTimePicker
-                      name="departureDate"
-                      label="Departure Date"
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Textfield
-                      name="message"
-                      label="Message"
-                      multiline={true}
-                      rows={4}
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Checkbox
-                      name="termsOfService"
-                      legend="Terms Of Service"
-                      label="I agree"
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Button>
-                      Submit Form
-                    </Button>
-                  </Grid>
-
-
-                </Grid>
-
-              </Form>
+                </Form>
+              )}}
             </Formik>
 
           </div>
         </Container>
-      </Grid>
-    </Grid>
   );
 };
